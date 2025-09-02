d35bf0766f0727d00463fa7da29f8a62aa28967f
Sfoglia-Massas-Artesanais <154479627+Sfoglia-Massas-Artesanais@users.noreply.github.com>
2025-09-01 23:40:39 -0300

    Delete js directory

--- js/toast.js
+++
@@ -1,9 +0,0 @@
-function showToast(message, duration = 3000) {
-  const toast = document.getElementById("toast");
-  toast.textContent = message;
-  toast.className = "toast show";
-
-  setTimeout(() => {
-    toast.className = "toast";
-  }, duration);
-}
